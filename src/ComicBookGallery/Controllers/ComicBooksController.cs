779aecf6667d24d9197dfc945462e107dbb75107
GARY H <garyholc1@hotmail.com>
2018-05-18 21:12:30 +0100

    added comic book respository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -9,25 +9,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController: Controller
     {
-        public ActionResult Detail()
-        {
-
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of vengence! Even if Spider-man survives....<strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                    {
-                        new Artist() { Role = "Script", Name = "Dan Slott"},
-                        new Artist() { Role = "Pencils", Name = "Humberto Ramos"},
-                        new Artist() { Role = "Inks", Name = "Victor Olazab"},
-                        new Artist() { Role = "Colors", Name = "Edgar Delgado"},
-                        new Artist() { Role = "Letters", Name = "Chris Eliopoulos"},
-                    }
-            };
-
-            return View(comicBook);
-        }
+        //public ActionResult Detail()
+        //{
+        //    return View(comicBook);
+        //}
     }
 }
